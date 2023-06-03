3315d3b4106357cd6fda3c83a8d270537f52fb8d
Ruben Almendares <57120636+Rublox99@users.noreply.github.com>
2023-06-02 23:35:38 -0600

    Aplicación React principal

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client' //Elementos event del DOM
 import './index.css' //Exporta una hoja de estilos
 import App from './App.jsx' //Importa el .jsx y lo coloca en la función .render de ReactDOM
 
-//se trata de una aplicación Javascript que se inyecta en un .html
+//se trata de una aplicación Javascript que se inyecta en un .html 
 ReactDOM.createRoot(document.getElementById('root')).render( //root APUNTA AL <BODY> DE index.html
   <React.StrictMode>
     <App />
